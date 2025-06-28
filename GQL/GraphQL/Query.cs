c01bfddd762d07fcbbdc19b588ca902450abd928
Dzmitry <chukhno.d@ya.ru>
2025-06-28 15:37:33 +0300

    Added queries by roles

--- GQL/GraphQL/Query.cs
+++ GQL/GraphQL/Query.cs
@@ -1,19 +1,54 @@
 ﻿using GQL.Data;
 using GQL.Models;
+using HotChocolate.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace GQL.GraphQL;
 
 public class Query
 {
-    [UseProjection]
+
+    [Authorize]
+    [UsePaging(IncludeTotalCount = true)]
     [UseFiltering]
     [UseSorting]
     public IQueryable<TaskItem> GetTasks([Service] AppDbContext context)
-        => context.Tasks;
+    {
+        return context.Tasks.Include(t => t.CreatedBy);
+    }
 
+
+    [Authorize]
+    public async Task<TaskItem?> GetTaskById(int id, ClaimsPrincipal user, [Service] AppDbContext context)
+    {
+        var task = await context.Tasks
+            .Include(t => t.CreatedBy)
+            .FirstOrDefaultAsync(t => t.Id == id);
+
+        if (task == null)
+            return null;
+
+        var userId = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+        var userRole = user.FindFirst(ClaimTypes.Role)?.Value;
+
+        if (userRole == "Admin" || task.CreatedById == userId)
+            return task;
+
+        throw new GraphQLException(ErrorBuilder.New()
+            .SetMessage("Access denied.")
+            .SetCode("FORBIDDEN")
+            .Build());
+    }
+
+
+    [Authorize(Roles = ["Admin" ])]
     [UseProjection]
     [UseFiltering]
     [UseSorting]
     public IQueryable<User> GetUsers([Service] AppDbContext context)
-       => context.Users;
+    {
+        return context.Users;
+    }
+
 }
